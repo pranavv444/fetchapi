2eff7d86857e2b775b64ebaeb038781ec7a716b7
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-07-23 02:18:27 +0530

    4th commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-fetch('https://jsonplaceholder.typicode.com/comments').then(response=>{
+fetch('https://jsonplaceholder.typicode.com/posts').then(response=>{
     if(!response.ok){
         throw new Error('Network not responding');
     }
-    return response.text();
+    return response.json();
 })
-.then(data=>console.log(data))
-.catch(error=>console.log(error))
+.then(data=>data.forEach(element=>{console.log(element.title)})
+.catch(error=>console.log(error)));
